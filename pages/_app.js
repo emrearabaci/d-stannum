0862ee04a846d273e89327f4c910d1cec0082976
Emre Arabaci <a.emrearabaci@gmail.com>
2022-01-23 17:34:23 +0300

    Update _app.js
    "Bootstrap 5.1.3" added for project structure.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+/* Next Modules */
+import { useEffect } from 'react';
+
+/* Libraries scss, js files */
 import 'normalize.css/normalize.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+/* Custom scss and js files */
 import '../styles/styles.scss';
 
-// This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    import('bootstrap/dist/js/bootstrap.bundle.min');
+  }, []);
+
   return <Component {...pageProps} />;
 }
